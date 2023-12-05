38d5821acc961bca85406d2bf94d6591a800b5c6
Umer Farooq <98796369+umerfarooq777@users.noreply.github.com>
2023-12-05 20:18:41 +0500

    all functions done ready to audit and publish

--- src/sdk.js
+++ src/sdk.js
@@ -1,8 +1,11 @@
-const { ethers } = require('ethers');
+const { ethers} = require('ethers');
+const BigNumber = require('bignumber.js');
+
 require('dotenv').config();
 const axios = require('axios');
 const ABI = require("./DefContractABI.json")
 const testABI = require("./testABI.json")
+const poolABI = require("./poolABI.json")
 
 
 
@@ -29,6 +32,8 @@ class defTokenSDK {
 
             this.DefContract = new ethers.Contract(contractAddress, contractABI, this.wallet);
 
+            
+
             // this.getContract();
 
         } catch (error) {
@@ -37,10 +42,9 @@ class defTokenSDK {
         }
     }
 
-    // isWholeNumber(str) {
-    //     const num = Number(str);
-    //     return !isNaN(num) && num % 1 !== 0;
-    //   }
+    getSigner() {
+        return this.wallet
+      }
 
 
     async checkIsOwner() {
@@ -53,6 +57,156 @@ class defTokenSDK {
        
       }
 
+    async validateUserTokenAllowance(amount,_ownerAddress,_spenderAddress) {
+            if ((_ownerAddress&&!_spenderAddress) || (_spenderAddress && !_ownerAddress)){
+
+                throw new Error(`Please provide owner and spender address`);
+            }
+            
+            if (Number(amount)<=0) {
+                
+                throw new Error(`amount (${amount}) must be greater than 0 `);
+            }
+
+            // console.log(await this.wallet.getAddress(),await this.DefContract.getAddress());
+            const userAllowanceToContract = await this.getTokenAllowance( _ownerAddress|| await this.wallet.getAddress(),_spenderAddress||await this.DefContract.getAddress());
+            
+            const amountToSwap = BigNumber(amount.toString());
+            const userFormattedAllowance = BigNumber(userAllowanceToContract.toString());
+            
+            const isAllowanceGreaterOrEqual = userFormattedAllowance.gte(amountToSwap);
+      
+            if (!isAllowanceGreaterOrEqual) {
+                throw new Error(`Insufficient token allowance : current allowance (${userAllowanceToContract.toString()}) `)
+            }
+
+        
+       
+      }
+
+
+    async validateUserLPTokenAmount(amount) {
+ 
+        
+        //!== to be added 
+           if (Number(amount)<=0) {
+                
+                throw new Error(`amount (${amount}) must be greater than 0 `);
+            }
+
+            const poolAddress =  await this.DefContract.getPoolAddress(); 
+            const poolContractABI = poolABI;
+            const poolContract = new ethers.Contract(poolAddress, poolContractABI, this.wallet);
+
+            const userLPBalance = await poolContract.balanceOf(this.wallet.address);
+            
+            const amountToRemove = new BigNumber(amount.toString());
+            const userBalanceAvailable = new BigNumber(userLPBalance.toString());
+            
+            const isBalanceGreaterOrEqual = userBalanceAvailable.gte(amountToRemove); // userBalanceAvailable >= amountToRemove 
+            
+            if (!isBalanceGreaterOrEqual) {
+                throw new Error(`Insufficient LP balance : user LP balance (${userLPBalance.toString()}) `)
+            }
+            
+        
+       console.log("done");
+      }
+    async validateUserLPTokenAllowance(amount) {
+ 
+        
+        //!== to be added 
+           if (Number(amount)<=0) {
+                
+                throw new Error(`amount (${amount}) must be greater than 0 `);
+            }
+
+            const poolAddress =  await this.DefContract.getPoolAddress(); 
+            const poolContractABI = poolABI;
+            const poolContract = new ethers.Contract(poolAddress, poolContractABI, this.wallet);
+
+            const userLPAllowance = await poolContract.allowance(await this.wallet.getAddress(),await this.DefContract.getAddress());
+            
+            const amountToRemove = new BigNumber(amount.toString());
+            const userBalanceAvailable = new BigNumber(userLPAllowance.toString());
+            
+            const isBalanceGreaterOrEqual = userBalanceAvailable.gte(amountToRemove); // userBalanceAvailable >= amountToRemove 
+            
+            if (!isBalanceGreaterOrEqual) {
+                throw new Error(`Insufficient LP allowance : user LP allowance (${userLPAllowance.toString()}) `)
+            }
+            
+        
+            console.log("done");
+       
+      }
+
+
+    async approveUserLPTokenAmount(spender,amount) {
+ 
+        
+        //!== to be added 
+           if (Number(amount)<=0) {
+                
+                throw new Error(`amount (${amount}) must be greater than 0 `);
+            }
+
+            const poolAddress =  await this.DefContract.getPoolAddress(); 
+            const poolContractABI = poolABI;
+            const poolContract = new ethers.Contract(poolAddress, poolContractABI, this.wallet);
+            
+            const approveRes = await poolContract.approve(spender|| await this.DefContract.getAddress(),amount);
+            return approveRes
+        
+       
+      }
+
+
+    async validateUserEthAmount(amount) {
+ 
+        
+           
+           if (Number(amount)<=0) {
+                
+                throw new Error(`amount (${amount}) must be greater than 0 `);
+            }
+            const userBalance = await this.provider.getBalance(this.wallet.address);
+
+            const amountToSwap = new BigNumber(amount.toString());
+            const userBalanceAvailable = new BigNumber(userBalance.toString());
+            
+            const isBalanceGreaterOrEqual = userBalanceAvailable.gte(amountToSwap); // userBalanceAvailable >= amountToSwap 
+            
+            if (!isBalanceGreaterOrEqual) {
+                throw new Error(`Insufficient eth balance : user balance (${userBalance.toString()}) `)
+            }
+            
+        
+       
+      }
+    async validateUserTokenAmount(amount,userAddress) {
+ 
+        
+           
+           if (Number(amount)<=0) {
+                
+                throw new Error(`amount (${amount}) must be greater than 0 `);
+            }
+            const userBalance = await this.getTokenBalanceOfAddress(userAddress || this.wallet.address);
+
+            const amountToSwap = new BigNumber(amount.toString());
+            const userBalanceAvailable = new BigNumber(userBalance.toString());
+            
+            const isBalanceGreaterOrEqual = userBalanceAvailable.gte(amountToSwap);
+            
+            if (!isBalanceGreaterOrEqual) {
+                throw new Error(`Insufficient token balance : user balance (${userBalance.toString()}) `)
+            }
+            
+        
+       
+      }
+
     //! =============== SDK Read Functions
 
     async getMyBalance() {
@@ -61,6 +215,15 @@ class defTokenSDK {
             return ethers.formatEther(balance.toString());
         } catch (error) {
             throw new Error(`Error getting balance: ${error.message}`);
+        }
+    }
+
+    async getETHBalanceOf(address) {
+        try {
+            const balance = await this.provider.getBalance(address);
+            return balance
+        } catch (error) {
+            throw new Error(`Error getETHBalanceOf: ${error.message}`);
         }
     }
 
@@ -176,8 +339,8 @@ class defTokenSDK {
     async getTokenAllowance(owner,spender) {
         try {
             
-            const balance = await this.DefContract.allowance(owner,spender);
-            return balance;
+            const _allowance = await this.DefContract.allowance(owner,spender);
+            return _allowance;
         } catch (error) {
             throw new Error(`Error getting getTokenAllowance: ${error.message}`);
         }
@@ -305,7 +468,7 @@ class defTokenSDK {
     async updateTaxFreeAddressesAccess(_addresses, _hasAccess) {
         try {
             
-            await this.checkIsOwner();
+        await this.checkIsOwner();
 
 
         let res = await this.DefContract.setIsTaxExcluded(_addresses, _hasAccess);
@@ -314,6 +477,131 @@ class defTokenSDK {
             throw new Error(`Error setNewTaxPercentage: ${error.message}`);
         }
     }
+
+
+    async transferContractOwnership(_newOwner) {
+        try {
+            
+        await this.checkIsOwner();
+
+
+        let res = await this.DefContract.transferOwnership(_newOwner);
+        return res
+
+        } catch (error) {
+            throw new Error(`Error transferContractOwnership: ${error.message}`);
+        }
+    }
+
+    async collectTaxedTokenAsEth() {
+        try {
+            
+        await this.checkIsOwner();
+
+
+        let res = await this.DefContract.withDrawTaxCollection();
+        return res
+
+        } catch (error) {
+            throw new Error(`Error collectTaxedTokenAsEth: ${error.message}`);
+        }
+    }
+
+
+    async swapTokenWithEth(tokenAmountToSwap) {
+        try {
+            
+        await this.validateUserTokenAmount(tokenAmountToSwap);
+        await this.validateUserTokenAllowance(tokenAmountToSwap);
+        
+        let res = await this.DefContract.swapTokenWithEth(tokenAmountToSwap);
+        return res
+
+        } catch (error) {
+            throw new Error(`Error swapTokenWithEth: ${error.message}`);
+        }
+    }
+    
+    async swapEthWithToken(ethAmountToSwap) {
+        try {
+            
+            await this.validateUserEthAmount(ethAmountToSwap);
+        
+            let res = await this.DefContract.swapEthWithToken({value:ethAmountToSwap.toString()});
+            return res
+            
+        } catch (error) {
+            throw new Error(`Error swapEthWithToken: ${error.message}`);
+        }
+    }
+
+
+    async transferDefToken(_recipent,tokenAmountToSend) {
+        try {
+            
+            
+        await this.validateUserTokenAmount(tokenAmountToSend);
+        
+        let res = await this.DefContract.transfer(_recipent,tokenAmountToSend);
+        return res
+    
+        } catch (error) {
+            throw new Error(`Error transferDefToken: ${error.message}`);
+        }
+    }
+
+
+    async transferDefTokenFrom(_from,_recipent,tokenAmountToSend) {
+        try {
+            
+            
+        await this.validateUserTokenAmount(tokenAmountToSend,_from);
+        await this.validateUserTokenAllowance(tokenAmountToSend,_from, await this.wallet.getAddress());
+        
+        let res = await this.DefContract.transferFrom(_from,_recipent,tokenAmountToSend);
+        return res
+    
+        } catch (error) {
+            throw new Error(`Error transferDefTokenFrom: ${error.message}`);
+        }
+    }
+
+
+    async addLiquidityETHToPool(tokenAmountToSend,ethWeiAmount) {
+        try {
+            
+            
+        await this.validateUserEthAmount(ethWeiAmount);
+        await this.validateUserTokenAmount(tokenAmountToSend);
+        
+        let res = await this.DefContract.addLiquidityETHToPool(tokenAmountToSend,{value: ethWeiAmount});
+        return res
+    
+        } catch (error) {
+            throw new Error(`Error transferDefTokenFrom: ${error.message}`);
+        }
+    }
+    
+    async removeLiquidityETHToPool(lpToken) {
+        try {
+            
+            
+        await this.validateUserLPTokenAmount(lpToken);
+        await this.validateUserLPTokenAllowance(lpToken);
+        
+        let res = await this.DefContract.removeLiquidityETHToPool(lpToken);
+        return res
+    
+        } catch (error) {
+            throw new Error(`Error removeLiquidityETHToPool: ${error.message}`);
+        }
+    }
+
+
+
+
+
+
 }
 
 module.exports = defTokenSDK;
--- test/app.js
+++ test/app.js
@@ -63,33 +63,192 @@ async function test() {
 
         //! WRITE
 
+        //! === 1
         //approves respective tokens to a spender
         // const Transaction0 = await defTokenSdkInstance.approveTokensTo(contractAddress,await getTokenToWei("333"));
         // const res = await Transaction0.wait();
         // console.log('getContract =>', await defTokenSdkInstance.getFormateEther(await defTokenSdkInstance.getTokenAllowance(ownerAddress, contractAddress)));
         
+        //! === 2
         //approves max tokens to def contract
         // const Transaction1 = await defTokenSdkInstance.ApproveMaxTokensToContract();
         // const res1 = await Transaction1.wait();
         // console.log('getContract =>', await defTokenSdkInstance.getFormateEther(await defTokenSdkInstance.getTokenAllowance(ownerAddress, contractAddress)));
         
+        //! === 3
         //set burn percentage by only owner 
         // console.log('getContract =>', (await defTokenSdkInstance.getContractDetails()).burnPercentage);
         // const Transaction1 = await defTokenSdkInstance.setNewBurnPercentage("2");
         // const res1 = await Transaction1.wait();
         // console.log('getContract =>', (await defTokenSdkInstance.getContractDetails()).burnPercentage);
         
+        //! === 4
         // set tax percentage by only owner 
         // console.log('getContract =>', (await defTokenSdkInstance.getContractDetails()).taxPercentage);
         // const Transaction1 = await defTokenSdkInstance.setNewTaxPercentage("8");
         // const res1 = await Transaction1.wait();
         // console.log('getContract =>', (await defTokenSdkInstance.getContractDetails()).taxPercentage);
         
+        //! === 5
         // set tax percentage by only owner 
-        console.log('getContract =>', (await defTokenSdkInstance.getContractDetails()).taxPercentage);
-        const Transaction1 = await defTokenSdkInstance.setNewTaxPercentage("8");
+        // console.log('getContract =>', (await defTokenSdkInstance.getContractDetails()).taxPercentage);
+        // const Transaction1 = await defTokenSdkInstance.setNewTaxPercentage("8");
+        // const res1 = await Transaction1.wait();
+        // console.log('getContract =>', (await defTokenSdkInstance.getContractDetails()).taxPercentage);
+        
+        //! === 6
+        // set addresses as tax able or tax free by only owner 
+        // const add1 = "0xf3545A1eaD63eD1A6d8b6E63d68D937cdBf1aeE4"
+        // const add2 = "0x5cbD5063DdaE154c546860e2A4D2C16E2e1C786c"
+        // const arrayOfAddresses = [add1,add2]
+        // for (let index = 0; index < arrayOfAddresses.length; index++) {
+        //     const userAddress = arrayOfAddresses[index];
+        //     console.log('user',userAddress,"is tax free?", (await defTokenSdkInstance.checkAddressIsTaxFree(userAddress)));
+        // } //before
+        // const Transaction1 = await defTokenSdkInstance.updateTaxFreeAddressesAccess(arrayOfAddresses,false);
+        // const res1 = await Transaction1.wait();
+        // for (let index = 0; index < arrayOfAddresses.length; index++) {
+            //     const userAddress = arrayOfAddresses[index];
+            //     console.log('user',userAddress,"is tax free?", (await defTokenSdkInstance.checkAddressIsTaxFree(userAddress)));
+            // } //after
+            
+        //! === 7
+        //set new owner
+        // const newOwnerAddress1 = "0x808f0597D8B83189ED43d61d40064195F71C0D15"
+        // const newOwnerAddress2 = "0xf3545A1eaD63eD1A6d8b6E63d68D937cdBf1aeE4"
+        // console.log(
+        //     "current owner address",
+        // (await defContract.owner())
+
+        // );
+        // const Transaction1 = await defTokenSdkInstance.transferContractOwnership(newOwnerAddress1);
+        // const res1 = await Transaction1.wait();
+        // console.log(
+        //     "current owner address",
+        // (await defContract.owner())
+
+        // );
+
+        //! === 8
+        //get collected taxed token as eth
+        // console.log("current contract Balance",(await defTokenSdkInstance.getETHBalanceOf(contractAddress)));
+        // console.log("current contract Token Balance",(await defTokenSdkInstance.getTokenBalanceOfAddress(contractAddress)));
+        // console.log("current owner Balance",(await defTokenSdkInstance.getETHBalanceOf(ownerAddress)));
+        // console.log("current owner Balance",(await defTokenSdkInstance.getTokenBalanceOfAddress(ownerAddress)));
+
+        // const Transaction1 = await defTokenSdkInstance.collectTaxedTokenAsEth();
+        // const res1 = await Transaction1.wait();
+
+        // console.log("current contract Balance",(await defTokenSdkInstance.getETHBalanceOf(contractAddress)));
+        // console.log("current contract Token Balance",(await defTokenSdkInstance.getTokenBalanceOfAddress(contractAddress)));
+        // console.log("current owner Balance",(await defTokenSdkInstance.getETHBalanceOf(ownerAddress)));
+        // console.log("current owner Balance",(await defTokenSdkInstance.getTokenBalanceOfAddress(ownerAddress)));
+        
+        //! === 9
+        //get swapped token with eth
+        // const testAmount = await getTokenToWei("1.999999999999999997")
+        // const userAddress = await defTokenSdkInstance.getSigner().getAddress();
+        // console.log(testAmount,userAddress);
+        // console.log("current userAddress Balance",(await defTokenSdkInstance.getETHBalanceOf(userAddress)));
+        // console.log("current userAddress Token Balance",(await defTokenSdkInstance.getTokenBalanceOfAddress(userAddress)));
+        // console.log("current userAddress Token Allowance",(await defTokenSdkInstance.getTokenAllowance(userAddress,contractAddress)));
+        
+        // const Transaction1 = await defTokenSdkInstance.swapTokenWithEth(testAmount.toString());
+        // const res1 = await Transaction1.wait();
+        
+        // console.log("current userAddress Balance",(await defTokenSdkInstance.getETHBalanceOf(userAddress)));
+        // console.log("current userAddress Token Balance",(await defTokenSdkInstance.getTokenBalanceOfAddress(userAddress)));
+        // console.log("current userAddress Token Allowance",(await defTokenSdkInstance.getTokenAllowance(userAddress,contractAddress)));
+        
+        //! === 10
+        //get swapped eth with def token
+        // const ethWeiAmount = await getTokenToWei("0.00000001")
+        // const userAddress = await defTokenSdkInstance.getSigner().getAddress();
+        // console.log(ethWeiAmount,userAddress);
+        // console.log("current userAddress Balance",(await defTokenSdkInstance.getETHBalanceOf(userAddress)));
+        // console.log("current userAddress Token Balance",(await defTokenSdkInstance.getTokenBalanceOfAddress(userAddress)));
+        
+        // const Transaction1 = await defTokenSdkInstance.swapEthWithToken(ethWeiAmount.toString());
+        // const res1 = await Transaction1.wait();
+        
+        // console.log("current userAddress Balance",(await defTokenSdkInstance.getETHBalanceOf(userAddress)));
+        // console.log("current userAddress Token Balance",(await defTokenSdkInstance.getTokenBalanceOfAddress(userAddress)));
+        
+        //! === 11
+        //transfer def token to an address
+        // const tokenAmount = await getTokenToWei("500")
+        // const userAddress = await defTokenSdkInstance.getSigner().getAddress();
+        // const recipent = "0xf3545A1eaD63eD1A6d8b6E63d68D937cdBf1aeE4";
+        // console.log(tokenAmount,userAddress);
+        // console.log("current userAddress Token Balance",(await defTokenSdkInstance.getTokenBalanceOfAddress(userAddress)));
+        // console.log("current recipent Token Balance",(await defTokenSdkInstance.getTokenBalanceOfAddress(recipent)));
+        // const Transaction1 = await defTokenSdkInstance.transferDefToken(recipent,tokenAmount.toString());
+        // const res1 = await Transaction1.wait();
+        // console.log("current recipent Token Balance",(await defTokenSdkInstance.getTokenBalanceOfAddress(recipent)));
+        
+        //! === 12
+        //transfer def token from an address to another address
+        // const tokenAmount = await getTokenToWei("30")
+        // const userAddress = await defTokenSdkInstance.getSigner().getAddress();
+        // const fromAddress = "0xf3545A1eaD63eD1A6d8b6E63d68D937cdBf1aeE4";
+        // const recipent = "0x5cbD5063DdaE154c546860e2A4D2C16E2e1C786c";
+        // console.log(tokenAmount);
+        // console.log("current fromAddress Token Balance",(await defTokenSdkInstance.getTokenBalanceOfAddress(fromAddress)));
+        // console.log("current recipent Token Balance",(await defTokenSdkInstance.getTokenBalanceOfAddress(recipent)));
+        // console.log("current userAddress Token Allowance",(await defTokenSdkInstance.getTokenAllowance(fromAddress,userAddress)));
+        
+        // //from address must give allowance to function caller
+        // const Transaction1 = await defTokenSdkInstance.transferDefTokenFrom(fromAddress,recipent,tokenAmount.toString());
+        // const res1 = await Transaction1.wait();
+
+        // console.log("current fromAddress Token Balance",(await defTokenSdkInstance.getTokenBalanceOfAddress(fromAddress)));
+        // console.log("current recipent Token Balance",(await defTokenSdkInstance.getTokenBalanceOfAddress(recipent)));
+        // console.log("current userAddress Token Allowance",(await defTokenSdkInstance.getTokenAllowance(fromAddress,userAddress)));
+        
+        //! === 13
+        //add liquidity eth with token
+        // const tokenAmount = await getTokenToWei("200")
+        // const ethAmount = await getTokenToWei("0.00002")
+        // const userAddress = await defTokenSdkInstance.getSigner().getAddress();
+        // console.log(tokenAmount);
+        // console.log(ethAmount);
+        // console.log("current userAddress Token Balance",(await defTokenSdkInstance.getTokenBalanceOfAddress(userAddress)));
+        // console.log("current userAddress ETH Balance",(await defTokenSdkInstance.getETHBalanceOf(userAddress)));
+        // console.log("current userAddress Token Allowance",(await defTokenSdkInstance.getTokenAllowance(userAddress,contractAddress)));
+        // console.log("current userAddress LP Token Balance",(await defTokenSdkInstance.getLiquidityPoolTokensOf(userAddress)));
+        
+        // //caller account must give allowance to function contract address
+        // const Transaction1 = await defTokenSdkInstance.addLiquidityETHToPool(tokenAmount.toString(),ethAmount.toString());
+        // const res1 = await Transaction1.wait();
+        
+        // console.log("================================================ end of Transaction1================================");
+        // console.log("current userAddress Token Balance",(await defTokenSdkInstance.getTokenBalanceOfAddress(userAddress)));
+        // console.log("current userAddress ETH Balance",(await defTokenSdkInstance.getETHBalanceOf(userAddress)));
+        // console.log("current userAddress Token Allowance",(await defTokenSdkInstance.getTokenAllowance(userAddress,contractAddress)));
+        // console.log("current userAddress LP Token Balance",(await defTokenSdkInstance.getLiquidityPoolTokensOf(userAddress)));
+        
+        //! === 14
+        // remove liquidity eth with token
+        const lpTokenAmount = await getTokenToWei("0.000001")
+        const userAddress = await defTokenSdkInstance.getSigner().getAddress();
+        console.log("current userAddress Token Balance",(await defTokenSdkInstance.getTokenBalanceOfAddress(userAddress)));
+        console.log("current userAddress ETH Balance",(await defTokenSdkInstance.getETHBalanceOf(userAddress)));
+        console.log("current userAddress Token Allowance",(await defTokenSdkInstance.getTokenAllowance(userAddress,contractAddress)));
+        console.log("current userAddress LP Token Balance",(await defTokenSdkInstance.getLiquidityPoolTokensOf(userAddress)));
+        
+        //caller account must give allowance to function contract address
+        const Transaction1 = await defTokenSdkInstance.removeLiquidityETHToPool(lpTokenAmount.toString());
         const res1 = await Transaction1.wait();
-        console.log('getContract =>', (await defTokenSdkInstance.getContractDetails()).taxPercentage);
+        
+        console.log("================================================ end of Transaction1================================");
+        console.log("current userAddress Token Balance",(await defTokenSdkInstance.getTokenBalanceOfAddress(userAddress)));
+        console.log("current userAddress ETH Balance",(await defTokenSdkInstance.getETHBalanceOf(userAddress)));
+        console.log("current userAddress Token Allowance",(await defTokenSdkInstance.getTokenAllowance(userAddress,contractAddress)));
+        console.log("current userAddress LP Token Balance",(await defTokenSdkInstance.getLiquidityPoolTokensOf(userAddress)));
+
+
+
+
 
 
     } catch (error) {
